1e0c356140ccc839eed887f41be24e55bbb3ead7
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-05-13 15:42:46 +0200

    Initialisation de deux variables 'a' et 'b' avec les valeurs 8 et 12

---
+++ jour1/job02/index.js
@@ -0,0 +1,9 @@
+// Déclaration des variables
+let a = 8;
+let b = 12;
+
+// Addition des variables
+let resultat = a + b;
+
+// Affichage du résultat
+console.log("Le résultat de l'addition de", a, "+", b, "est : ", resultat);
