c735329c392c681bc4b996caab77b735f4645bcd
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-05-13 16:00:44 +0200

    Utilisation de la méthode 'fs.readdir()' pour lire le contenu du répertoire courant. Filtrage des résultats pour n'afficher que les dossiers. Affichage des noms des dossiers dans la console

--- jour1/job04/index.js
+++ jour1/job04/index.js
@@ -1,3 +1,19 @@
 // Import du module fs
 const fs = require('fs');
 
+// Lecture du contenu du répertoire courant
+fs.readdir('.', {withFileTypes: true}, (err, files) => {
+    if (err) {
+        console.error('Erreur lors de la lecture du répertoire :', err);
+        return;
+    }
+
+    // Filtrage des dossiers
+    const directories = files.filter(file => file.isDirectory());
+
+    // Affichage des noms des dossiers
+    console.log("Dossiers présents dans le répertoire courant :");
+    directories.forEach(directory => {
+        console.log(directory.name);
+    });
+});
