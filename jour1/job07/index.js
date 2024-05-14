f4e04b559c36b33611e79aa09fb97489921972af
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-05-14 10:48:03 +0200

    Ajout de la lecture asynchrone du contenu du fichier 'data.txt'

--- jour1/job07/index.js
+++ jour1/job07/index.js
@@ -3,3 +3,13 @@ const fs = require('fs');
 
 // Chemin du fichier
 const filePath = 'C:/Users/lucym/runtrack-nodeJS/jour1/job07/data.txt';
+
+// Lecture asynchrone du contenu du fichier
+fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+        console.error("Erreur lors de la lecture du fichier :", err);
+        return;
+    }
+    console.log("Contenu du fichier data.txt :");
+    console.log(data);
+});
