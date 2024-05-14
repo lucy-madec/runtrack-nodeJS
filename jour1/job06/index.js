56c00f6e2f1a3f6c12952a497dba41efa05b4fc9
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-05-14 10:36:18 +0200

    Ajout de la lecture synchrone du contenu du fichier 'data.txt'

--- jour1/job06/index.js
+++ jour1/job06/index.js
@@ -3,3 +3,12 @@ const fs = require('fs');
 
 // Chemin du fichier
 const filePath = 'C:/Users/lucym/runtrack-nodeJS/jour1/job06/data.txt'
+
+// Lecture synchrone du contenu du fichier
+try {
+    const content = fs.readFileSync(filePath, 'utf-8');
+    console.log("Contenu du fichier data.txt:");
+    console.log(content);
+} catch (err) {
+    console.error("Erreur lors de la lecture du fichier :", err);
+}
