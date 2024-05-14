2df6a2bd6773c75d545d41116c307731de814402
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-05-14 11:28:03 +0200

    Ajout du nouveau contenu à écrire dans le fichier 'data.txt'

--- jour1/job09/index.js
+++ jour1/job09/index.js
@@ -3,3 +3,6 @@ const fs = require('fs');
 
 // Chemin du fichier
 const filePath = 'C:/Users/lucym/runtrack-nodeJS/jour1/job09/data.txt';
+
+// Nouveau contenu à écrire dans le fichier
+const newContent = "Je manipule les fichiers avec un module node !";
