aa750ba4286e34e4d2cb4d7455cfcae317675908
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-05-14 11:30:39 +0200

    Ajout de l'écriture pour le fichier 'data.txt'

--- jour1/job09/index.js
+++ jour1/job09/index.js
@@ -6,3 +6,12 @@ const filePath = 'C:/Users/lucym/runtrack-nodeJS/jour1/job09/data.txt';
 
 // Nouveau contenu à écrire dans le fichier
 const newContent = "Je manipule les fichiers avec un module node !";
+
+// Écriture dans le fichier
+fs.writeFile(filePath, newContent, (err) => {
+    if (err) {
+        console.error("Erreur lors de l'écriture dans le fichier :", err);
+        return;
+    }
+    console.log("Contenu du fichier data.txt modifié avec succès !");
+});
