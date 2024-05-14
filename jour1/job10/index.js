7273e27818e638e6c5da6233d8ca9b5741675323
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-05-14 11:40:31 +0200

    Récupration du nom d'hôte

--- jour1/job10/index.js
+++ jour1/job10/index.js
@@ -7,3 +7,7 @@ const URL = "https://www.google.com&search=nodejs";
 // Récupération du protocole utilisé
 const protocol = url.parse(URL).protocol;
 console.log("Protocole utilisé :", protocol);
+
+// Récupération du nom d'hôte
+const hostname = url.parse(URL, true).query;
+console.log("Paramètres de l'URL :", params);
