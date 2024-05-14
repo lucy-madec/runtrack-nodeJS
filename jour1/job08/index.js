4d8d811cffbb76412f6b7a99f292c1dca3a4bd42
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-05-14 11:15:46 +0200

    Ajout de la lecture asynchrone du contenu du fichier 'data.txt' en affichant une lettre sur deux

--- jour1/job08/index.js
+++ jour1/job08/index.js
@@ -3,3 +3,20 @@ const fs = require('fs');
 
 // Chemin du fichier
 const filePath = 'C:/Users/lucym/runtrack-nodeJS/jour1/job08/data.txt';
+
+// Lecture asynchrone du contenu du fichier
+fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+        console.error("Erreur lors de la lecture du fichier :", err);
+        return;
+    }
+
+    // Affichage d'une lettre sur deux
+    let result = "";
+    for (let i = 0; i < data.length; i += 2) {
+        result += data[i];
+    }
+
+    console.log("Contenu du fichier data.txt :");
+    console.log(result);
+});
