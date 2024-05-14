cc421e9d998f5df969c9fc5e6c4d39176f01dcb7
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-05-14 11:38:46 +0200

    Récupération du protocole utilisé

--- jour1/job10/index.js
+++ jour1/job10/index.js
@@ -3,3 +3,7 @@ const url = require('url');
 
 // Définition de l'URL
 const URL = "https://www.google.com&search=nodejs";
+
+// Récupération du protocole utilisé
+const protocol = url.parse(URL).protocol;
+console.log("Protocole utilisé :", protocol);
