7a96fc7e0fa94722f26d246aff5205f5e88383f6
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-05-15 13:23:38 +0200

    Lancement du serveur

--- jour1/job11/index.js
+++ jour1/job11/index.js
@@ -11,3 +11,8 @@ const server = http.createServer((req, res) => {
     // Envoi la réponse
     res.end('Hello World!\n');
 });
+
+// Lancement du serveur
+server.listen(port, () => {
+    console.log("Serveur en cours d'exécution sur http://localhost:${port}/");
+});
