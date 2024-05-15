c6d521cfcc83f480c03ea3b494ab9a5492b83dc1
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-05-15 13:45:56 +0200

    Création du serveur

--- jour1/job12/index.js
+++ jour1/job12/index.js
@@ -5,3 +5,20 @@ const path = require('path');
 
 // Définition du port
 const port = 8888;
+
+// Création du serveur
+const server = http.createServer((req, res) => {
+    // Chemin vers le fichier index.html
+    const filePath = path.join(__dirname, 'index.html');
+
+    // Lecture du fichier index.html
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Erreur du serveur interne');
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        }
+    });
+});
