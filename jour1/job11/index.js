16021eec8d7278a3db4900aa10dad82a7e6bdd10
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-05-15 13:20:58 +0200

    Création du serveur

--- jour1/job11/index.js
+++ jour1/job11/index.js
@@ -3,3 +3,11 @@ const http = require('http');
 
 // Définition du port
 const port = 8888;
+
+// Création du serveur
+const server = http.createServer((req, res) => {
+    // Définition de l'en-tête de la réponse
+    res.writeHead(200, { 'Conternt-Type': 'text/plain' });
+    // Envoi la réponse
+    res.end('Hello World!\n');
+});
