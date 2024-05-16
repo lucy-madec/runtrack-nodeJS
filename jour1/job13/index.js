20ccacfce1cfefae30f39e9c510fca70853ef39e
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-05-16 09:30:11 +0200

    Création et lancement du serveur

--- jour1/job13/index.js
+++ jour1/job13/index.js
@@ -1,7 +1,40 @@
-// Importation des modules http et fs
+// Import des modules http et fs
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
 // Définition du port
 const port = 8888;
+
+// Création du serveur
+const server = http.createServer((req, res) => {
+    let filePath = '';
+
+    // Déterminer le chemin du fichier en fonction de l'URL
+    if (req.url === '/') {
+        filePath = path.join(__dirname, 'index.html');
+    } else if (req.url === '/about') {
+        filePath = path.join(__dirname, 'about.html');
+    } else {
+        // Si l'URL n'est ni '/' ni '/about', renvoyer une erreur 404
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 Not Found');
+        return;
+    }
+
+    // Lire et servir le fichier HTML approprié
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Erreur du serveur interne');
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        }
+    });
+});
+
+// Lancer le serveur
+server.listen(port, () => {
+    console.log(`Serveur en cours d'exécution sur http://localhost:${port}/`);
+});
