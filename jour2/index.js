8cd1c880540efba6d42a2eecd6048ddad15f0cbe
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-05-16 13:08:02 +0200

    Changement des guillemets dans le 'server.listen'

--- jour2/index.js
+++ jour2/index.js
@@ -3,5 +3,5 @@ const server = require('./server');
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-    console.log('Server is running on http://localhost:${PORT}');
+    console.log("Server en cours d'exécution sur http://localhost:${PORT}");
 });
