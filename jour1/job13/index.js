dafcfd8d08420ec505d79eac41a6d29453145fa5
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-05-16 09:24:29 +0200

    Définition du port

--- jour1/job13/index.js
+++ jour1/job13/index.js
@@ -1,5 +1,7 @@
-// Import des modules http et fs
+// Importation des modules http et fs
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
+// Définition du port
+const port = 8888;
