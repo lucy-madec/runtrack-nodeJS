37d53024f195062e1cea0efe569345659f283cd2
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-05-16 13:11:51 +0200

    Configuration et démarrage du serveur. Il importera les modules nécessaires et définira les middlewares

---
+++ jour2/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParder = require('body-parser');
+const routes = require('./routes');
+const exp = require('constants');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Middleware pour parser le JSON
+app.use(bodyParser.json());
+
+// Utilisation des routes définies dans routes.js
+app;use('/api', routes);
+
+module.exports = app;
