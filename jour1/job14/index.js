db8cf3391e5afc690870b3fd283c7766aa95647b
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-05-16 10:02:13 +0200

    Correction du code pour visualiser la page d'erreur

--- jour1/job14/index.js
+++ jour1/job14/index.js
@@ -1,16 +1,15 @@
-// Importation des modules http et fs
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
 // Définition du port
-const port = 8888;
+const port = 8888; // Par exemple, utilisez le port 8888
 
 // Création du serveur
 const server = http.createServer((req, res) => {
     let filePath = '';
 
-    // Détermination du chemin du fichier en fonction de l'URL
+    // Déterminer le chemin du fichier en fonction de l'URL
     if (req.url === '/') {
         filePath = path.join(__dirname, 'index.html');
     } else if (req.url === '/about') {
@@ -18,22 +17,22 @@ const server = http.createServer((req, res) => {
     } else {
         // Chemin vers la page d'erreur
         filePath = path.join(__dirname, 'error.html');
-        res.writeHead(404, { 'Content-Type': contentType });
     }
 
-    // Lecture et récupération du fichier HTML approprié
+    // Lire et servir le fichier HTML approprié
     fs.readFile(filePath, (err, data) => {
         if (err) {
             res.writeHead(500, { 'Content-Type': 'text/plain' });
             res.end('Erreur du serveur interne');
         } else {
+            let contentType = 'text/html';
             res.writeHead(200, { 'Content-Type': contentType });
             res.end(data);
         }
     });
 });
 
-// Lancement du serveur
+// Lancer le serveur
 server.listen(port, () => {
     console.log(`Serveur en cours d'exécution sur http://localhost:${port}/`);
 });
